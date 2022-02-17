659785254e476c00a28e61b90e352434bcd6394d
tsenguun224 <btseegii35@gmail.com>
2022-02-17 11:32:45 +0800

    1
    projectiig ehluulev

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen'
+import BikeRentScreen from './src/screens/BikeRentScreen'
+
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="Bike Rent" component={BikeRentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,6 @@
+import {View,Text,TouchableOpacity} from 'react-native'
+export default function HomeScreen(){
+    return <View>
+        <Text>This is home screen</Text>
+    </View>   
+}
